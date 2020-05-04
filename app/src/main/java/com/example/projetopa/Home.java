ce60451563d53ae1896abd8e62508fcb6fddb7b8
LuisBarros <la94052@gmail.com>
2020-05-04 21:33:14 +0100

    changes 3

--- app/src/main/java/com/example/projetopa/Home.java
+++ app/src/main/java/com/example/projetopa/Home.java
@@ -2,13 +2,54 @@ package com.example.projetopa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+    private Button addPacient = (Button)findViewById(R.id.addpaciente);
+    private Button editPaciente = (Button)findViewById(R.id.editpaciente);
+    private Button delPaciente = (Button)findViewById(R.id.delpaciente);
+    private Button verTabela = (Button)findViewById(R.id.vertabela);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        addPacient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, AddPaciente.class);
+                startActivity(intent);
+            }
+        });
+
+        editPaciente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, EditPaciente.class);
+                startActivity(intent);
+            }
+        });
+
+        delPaciente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, DelPaciente.class);
+                startActivity(intent);
+            }
+        });
+
+        verTabela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, VerTabela.class);
+                startActivity(intent);
+            }
+        });
     }
 }
