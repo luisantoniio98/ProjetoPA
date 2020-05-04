b688529576c75aa1b07d1f9a580b118e92b3525b
LuisBarros <la94052@gmail.com>
2020-05-04 21:44:52 +0100

    changes 3

--- app/src/main/java/com/example/projetopa/MainActivity.java
+++ app/src/main/java/com/example/projetopa/MainActivity.java
@@ -25,6 +25,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },1000);
+        },1500);
     }
 }
